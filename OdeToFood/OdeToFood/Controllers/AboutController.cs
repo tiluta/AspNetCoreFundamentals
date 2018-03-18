8c6806ef1f269f491e33e39395204aff5dbe8c27
tiluta <32142678+tiluta@users.noreply.github.com>
2018-03-18 15:36:25 +0200

    Create a new restaurant

---
+++ OdeToFood/OdeToFood/Controllers/AboutController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Controllers
+{
+    //about
+    //token controller
+    [Route("company/[controller]/[action]")]
+    public class AboutController
+    {
+        public string Phone()
+        {
+            return "1+555+555+5555";
+        }
+
+        public string Adress()
+        {
+            return "USA";
+        }
+
+    }
+}
